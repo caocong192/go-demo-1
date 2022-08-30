1992e1c583e184afa06bd1687e444b8c25133530
caocong <caocong@jiaxun.com>
2022-08-30 17:23:13 +0800

    支持系统中断和浏览器退出 同时生效

--- define/define.go
+++ define/define.go
@@ -1,15 +1,7 @@
 package define
 
 var (
-<<<<<<< HEAD
+
 	Port              = "27149"
 	DefaultUploadPath = "uploads"
 )
-=======
-	Port = "27149"
-	DefaultUploadPath = "uploads"
-)
-
-
-
->>>>>>> c875f6b522271ef460e14ac2f54da591f25d97e1
--- server/server.go
+++ server/server.go
@@ -1,18 +1,6 @@
 package server
 
 import (
-<<<<<<< HEAD
-	"github.com/go-demo-1/define"
-	"github.com/go-demo-1/server/routers"
-)
-
-// Run 启动gin
-func Run() {
-	// 设置路由
-	r := routers.SetRouters()
-
-	// 启动服务
-=======
 	"embed"
 	"github.com/gin-gonic/gin"
 	"github.com/go-demo-1/controller"
@@ -59,6 +47,6 @@ func Run()  {
 		}
 	})
 
->>>>>>> c875f6b522271ef460e14ac2f54da591f25d97e1
+
 	r.Run(":" + define.Port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
